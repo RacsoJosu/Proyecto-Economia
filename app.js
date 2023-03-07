427eafa4e48b137dfe5772c7a95d79ecb3600d02
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2023-03-07 00:21:57 -0600

    V 0.0.1

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3000 ;
+app.get('/',(req, res)=>{
+    res.send("Hola Mundo")
+})
+app.listen(3000);
+console.log("Servidor en el puerto 3000")
