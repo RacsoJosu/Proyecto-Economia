859874f06983848e443b970d1d5a8b6389018c4a
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2023-03-19 21:51:52 -0600

    V 0.0.3

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const webpack = require("webpack");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackConfig = require('../webpack.config');
+const port = process.env.PORT || 3000 ;
+
+const app = express();
+
+app.use(cors());
+app.set('port',port);
+app.set('./static',express.static('dist'))
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+
+app.get('/', function (req,res,next){
+    res.send('Ewebik')
+});
+
+app.listen(app.get('port'),()=>{
+    console.log("Servidor en el puerto 3000")
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const webpack = require("webpack");
+const htmlwebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode:'development',
+    entry: './views/js/index.js',
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+
+        ]
+    },
+    plugins:[
+        new htmlwebpackPlugin({
+            template:'./views/index.html'
+        })
+    ]
+}
+
